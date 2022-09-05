acc32f74a9ae6c530b198916a7e03d9f1a38f142
ol-kis <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>
2022-09-05 11:44:19 +0300

    создание массивов инициализация двух массиво

---
+++ Hw.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("Hello, You are runnung my litlle App");
+Console.WriteLine("Write some words");
+string [] words= Console.ReadLine().Split(',');
+string [] words_Length= new string[words.Length];
+
+
