f3a12ef1ac8a187f7b9ed6329ef009de918a2ff7
ol-kis <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>
2022-09-05 11:19:17 +0300

    создание второго массива

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, You are runnung my litlle App");
+Console.WriteLine("Write some words");
+string [] words= Console.ReadLine().Split(',');
+string [] words_Length= new string[words.Length];
+
+ 
