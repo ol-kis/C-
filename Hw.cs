2c1171e7b0f3775157f85fff3c9dc7b2f9b6866f
ol-kis <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>
2022-09-06 19:22:12 +0300

    полный код

--- Hw.cs
+++ Hw.cs
@@ -2,5 +2,15 @@ Console.WriteLine("Hello, You are runnung my litlle App");
 Console.WriteLine("Write some words");
 string [] words= Console.ReadLine().Split(',');
 string [] words_Length= new string[words.Length];
+<<<<<<< Updated upstream:Hw.cs
 
+=======
+for (int i=0;i<words.Length;i++){
+    if (words[i].Length<=3){
+        Console.Write($"{words[i]} ");
+        words_Length[i]=words[i];
+    }
+        }
+        Console.WriteLine(); 
+>>>>>>> Stashed changes:HW.cs
 
